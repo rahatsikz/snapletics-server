10bcd9b18a8c87602d96a146255fad0e4e27093e
rahatsikz <rascal6271@gmail.com>
2023-02-10 02:46:59 +0600

    service limit added

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htrvoxc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("Snapletics").collection("services");
+
+    app.get("/servicebylimit", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+
+run().catch((error) => console.error(error));
 
 app.get("/", (req, res) => {
   res.send("Snapletics server running");
